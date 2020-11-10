62b5668e9a08d383b38408622b85b63e51437a38
Jackson Grether <jackgrether@gmail.com>
2020-11-10 09:04:12 -0500

    initial commit

---
+++ myCV/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-cv';
+  Name = 'Jackson Grether'
+  Address = ' 4860 Avondale Ridge Drive'
+  Phone= ' 614-849-2716'
+  Email= ' Jackgrether@gmail.com'
+  WorkExperience =' Homeless'
+}
